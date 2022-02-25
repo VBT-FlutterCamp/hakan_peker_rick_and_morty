08d3d80c02792cb66c172d6db05a48da6a05ad38
hakannpeker <pekerhakan5@gmail.com>
2022-02-25 21:25:27 +0300

    circular progress indicator eklendi

--- lib/views/rick_morties/rick_morties_view.dart
+++ lib/views/rick_morties/rick_morties_view.dart
@@ -2,28 +2,38 @@ import 'package:flutter/material.dart';
 import 'rick_morties_view_model.dart';
 
 class RickMortiesView extends RickMortiesViewModel {
+  bool isLoading = false;
+  void changeLoading() {
+    isLoading = !isLoading;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        fetchAllData();
-      }),
-      appBar: AppBar(title: Text("Rick and Morty"),),
-      body: ListView.builder(
-        itemCount: models.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(models[index].image ?? 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'),
-              ),
-              title: Text(models[index].name ?? ""),
-              subtitle: Text(models[index].created ?? ""),
-              trailing: Text(models[index].episode ?? ""),
-            ),
-          );
-        },
-      ),
-    );
+        floatingActionButton: FloatingActionButton(onPressed: ()  {
+          changeLoading();
+
+          fetchAllData();
+        }),
+        appBar: AppBar(
+          title: Text("Rick and Morty"),
+        ),
+        body: isLoading
+            ? ListView.builder(
+                itemCount: models.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(models[index].image ?? 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'),
+                      ),
+                      title: Text(models[index].name ?? ""),
+                      subtitle: Text(models[index].created ?? ""),
+                      trailing: Text(models[index].episode ?? ""),
+                    ),
+                  );
+                },
+              )
+            : Center(child: CircularProgressIndicator()));
   }
 }
